53e5f5007cd88ad99c1d4edffbb041b5bc831976
abuziming <255670851@qq.com>
2023-02-22 21:49:25 +0800

    Initial commit

---
+++ common/service_utils/src/main/java/com/atguigu/ggkt/System/RedisCacheKey.java
@@ -0,0 +1,6 @@
+package com.atguigu.ggkt.System;
+
+public class RedisCacheKey {
+    public static final String Course_Key="course:user:";
+    public static final Long Course_TIME_TLL=30L;
+}
---
+++ service/service_activity/src/main/java/com/atguigu/ggkt/activity/service/CouponUseService.java
@@ -0,0 +1,17 @@
+package com.atguigu.ggkt.activity.service;
+
+import com.atguigu.ggkt.model.activity.CouponUse;
+import com.atguigu.ggkt.result.Result;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 优惠券领用表 服务类
+ * </p>
+ *
+ * @author atguigu
+ * @since 2022-04-29
+ */
+public interface CouponUseService extends IService<CouponUse> {
+
+}
